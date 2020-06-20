95f9ad538732e0cea884bf1a9548189313cad492
Tamal Saha <tamal@appscode.com>
2020-06-20 01:11:17 -0700

    Update update-release-tracker.sh
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/scripts/update-release-tracker.sh
+++ hack/scripts/update-release-tracker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright The KubeDB Authors.
+# Copyright The Kubeshield Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
